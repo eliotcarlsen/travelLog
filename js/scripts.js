d94ad1d37a9b069db38e12f0817fc20fadbe54f6
Eliot Carlsen and Xing Li <eliot@>
2017-02-27 10:13:42 -0800

    prototype

--- js/scripts.js
+++ js/scripts.js
@@ -5,29 +5,33 @@ function place (name, landmarks, date){
 };
 // nameArray = ['Seattle', 'San Francisco', 'San Diego', 'Siurana'];
 // landmarkArray = ["Space Needle", "Golden Gate Bridge", "The Beach", "Climbing"];
+place.prototype.note = function(){
+  return "We saw (the) " + this.landmarks + " in " + this.cityName + ".";
+};
 
 var placeSeattle = new place ("Seattle", "Space Needle", "2006");
 var placeSanFrancisco = new place ("San Francisco", "Golden Gate Bridge", "2016");
 var placeSanDiego = new place ("San Diego", "The Beach", "2009");
-var placeSiurana = new place ("Siurana", "climbing", "2017")
+var placeSiurana = new place ("Siurana", "climbing", "2017");
 
+console.log(placeSeattle.note());
 
 $(document).ready(function(){
   $(".seattle").click(function(event){
     event.preventDefault();
-    $("#seattle").append("We saw the " + placeSeattle.landmarks + " in " + placeSeattle.cityName + ".");
+    $("#seattle").append(placeSeattle.note());
     });
   $(".sanfrancisco").click(function(event){
     event.preventDefault();
-    $("#sanfrancisco").append("We saw the " + placeSanFrancisco.landmarks + " in " + placeSanFrancisco.cityName + ".");
+    $("#sanfrancisco").append(placeSanFrancisco.note());
   });
   $(".sandiego").click(function(event){
     event.preventDefault();
-    $("#sandiego").append("We saw " + placeSanDiego.landmarks + " in " + placeSanDiego.cityName + ".");
+    $("#sandiego").append(placeSanDiego.note());
   });
   $(".siurana").click(function(event){
     event.preventDefault();
-    $("#siurana").append("We went " + placeSiurana.landmarks + " in " + placeSiurana.cityName + ".");
+    $("#siurana").append(placeSiurana.note());
   });
 
 
